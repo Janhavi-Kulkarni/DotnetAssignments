ca293e358194ddf67bad3a84d68c87e00d1720b6
Janhavi-Kulkarni <janhavi.kulkarni@nitorinfotech.com>
2019-05-21 11:18:49 +0530

    assignment commit

---
+++ SeleniumAssignment/Practice.cs
@@ -0,0 +1,16 @@
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumAssignment
+{
+    class Practice
+    {
+        IWebDriver driver;
+
+        public void demo()
+        {
+            
+        }
+    }
+}
