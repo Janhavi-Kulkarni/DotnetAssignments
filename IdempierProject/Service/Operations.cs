5c3eae81a071a0fc2ec3eed8b8cbc86b3488a80c
Janhavi-Kulkarni <janhavi.kulkarni@nitorinfotech.com>
2019-05-28 18:16:00 +0530

    idempier

---
+++ IdempierProject/Service/Operations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Service
+{
+    public class Operations
+    {
+        IWebDriver driver;
+
+        public void InitializeBrowser()
+       { 
+           FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\CSharpGit\DotnetAssignments\SeleniumAssignment\drivers");
+           // service.FirefoxBinaryPath = @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
+            driver = new FirefoxDriver(service);
+           
+            
+
+       }
+    }
+}
